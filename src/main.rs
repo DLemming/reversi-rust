e9d0112d75389cf491df3f06e2c9647299e68167
David Lemming <david.lemming28@gmail.com>
2025-05-16 16:55:23 +0200

    Refactor main to use engine

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,42 @@ use std::io::{self, Write};
 mod game;
 use game::game::GameState;
 use game::r#move::Move;
+use game::player::Player;
+
+mod engine;
+use engine::engine::Engine;
 
 
 fn main() {
-    let mut game = GameState::new();
+    let mut game: GameState = GameState::new();
+    let engine = Engine::new(15);
+    println!("{}", game);
+
+    while !game.game_over() {
+        println!("Current player: {}", game.current_player());
+
+        // Get current player's move
+        let mv: u8 = match game.current_player() {
+            Player::White => get_engine_move(&game, &engine),
+            Player::Black => get_human_move(&game)
+        };
+
+        game.apply_move(mv);
+        game.switch_player();
+        println!("{}", game);
+    }
+
+    match game.winner() {
+        Some(player) => println!("Winner: {}", player),
+        None => println!("It's a draw!"),
+    }
+}
+
+fn get_human_move(game: &GameState) -> u8 {
+    println!("Enter your move (e.g., E6):");
+    let mut mv: u8;
 
     loop {
-        // Show game
-        println!("{}", game);
-
-        if game.game_over {
-            match game.winner() {
-                Some(player) => println!("Winner: {}", player),
-                None => println!("It's a draw!"),
-            }
-            break;
-        }
-        println!("{} to move. Enter your move (e.g., E6):", game.to_move);
-
         print!("> ");
         io::stdout().flush().unwrap();
 
@@ -30,20 +48,39 @@ fn main() {
         }
 
         let trimmed = input.trim();
-        let mv = Move::new(trimmed);
-        // let mv: Move = match Move::new(trimmed) {
-        //     Some(mv) => mv,
-        //     None => {
-        //         println!("Invalid move format, try again.");
-        //         continue;
-        //     }
-        // };
 
-        if (game.board.valid & (1u64 << mv.0)) == 0 {
+        mv = match Move::new(trimmed) {
+            Some(mv) => mv.0,
+            None => {
+                println!("Invalid move format, try again.");
+                continue;
+            }
+        };
+
+        if (game.board.legal_moves(game.current_player().to_bool()) & (1u64 << mv)) == 0 {
             println!("Illegal move. Try again.");
             continue;
         }
-
-        game.play_move(mv);
+        break;
     }
+    
+    mv
+}
+
+fn get_engine_move(game: &GameState, engine: &Engine) -> u8 {
+    let (score, mv) = engine.search(game);
+
+    let mv: u8 = match mv {
+        Some(mv) => mv,
+        None => {
+            println!("ERROR. Engine did not find a move!");
+            0
+        }
+    }; 
+
+    println!("Engine played: {}. Score in {} moves: {}.\n", Move::to_str(mv), engine.depth, score);
+    println!("Nodes searched: {}", engine.node_counter.borrow());
+    println!("Time elapsed: {:.2?}", engine.last_search_time.borrow());
+
+    mv
 }

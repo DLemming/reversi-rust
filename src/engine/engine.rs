2df7a8e5871b2e1ddc2fc0609ec66b45c8361b31
David Lemming <david.lemming28@gmail.com>
2025-05-16 16:54:34 +0200

    Implement engine

---
+++ src/engine/engine.rs
@@ -0,0 +1,92 @@
+use std::cell::RefCell;
+use std::time::{Duration, Instant};
+
+use crate::game::game::GameState;
+use crate::game::board::{Bitboard, BitIter};
+use crate::engine::node::Node;
+
+pub struct Engine {
+    pub depth: u8, // Max depth to search
+    pub node_counter: RefCell<usize>, // <- shared mutability, no threading
+    pub last_search_time: RefCell<Duration>,
+}
+
+impl Engine {
+    pub fn new(depth: u8) -> Self {
+        Engine {
+            depth,
+            node_counter: RefCell::new(0),
+            last_search_time: RefCell::new(Duration::ZERO)
+        }
+    }
+
+    pub fn search(&self, state: &GameState) -> (i8, Option<u8>) {
+        *self.node_counter.borrow_mut() = 0;
+        let start_time = Instant::now();
+
+        let is_white = state.current_player().to_bool();
+        let node = Node::new(state.board, is_white, state.board.legal_moves(is_white));
+
+        let mut best_move: Option<u8> = None;
+        let mut best_score = if node.is_white { i8::MIN } else { i8::MAX };
+
+        // use custom Bit iterator
+        for bit in BitIter(node.legal_moves) {
+            let score = self.minimax(
+                node.apply_move(bit),
+                self.depth - 1,
+                i8::MIN,
+                i8::MAX
+            );
+
+            let is_better = (node.is_white && score > best_score)
+                        || (!node.is_white && score < best_score);
+            
+            if is_better {
+                best_score = score;
+                best_move = Some(bit);
+            }
+        }
+
+        let duration = start_time.elapsed();
+        *self.last_search_time.borrow_mut() = duration;
+
+        (best_score, best_move)
+    }
+    
+    fn minimax(&self, node: Node, depth: u8, mut alpha: i8, mut beta: i8) -> i8 {
+        *self.node_counter.borrow_mut() += 1;
+
+        // depth 0 or game over
+        if (depth == 0) || (node.legal_moves == 0) {
+            return self.static_eval(&node.board);
+        }
+
+        let mut best_eval = if node.is_white { i8::MIN } else {i8::MAX };
+
+        for bit in BitIter(node.legal_moves) {
+            let eval = self.minimax(node.apply_move(bit), depth - 1, alpha, beta);
+            
+            if node.is_white {
+                best_eval = best_eval.max(eval);
+                alpha = alpha.max(best_eval);
+            } else {
+                best_eval = best_eval.min(eval);
+                beta = beta.min(best_eval);
+            }
+            
+            if beta <= alpha {
+                return best_eval; // PRUNE!
+            }
+        }
+
+        best_eval
+    }
+    
+    fn static_eval(&self, board: &Bitboard) -> i8 {
+        let (white, black) = board.score();
+
+        // white wants >0 (maximizing), black wants <0 (minimizing)
+        return white as i8 - black;
+    }
+}
---
+++ src/engine/node.rs
@@ -0,0 +1,34 @@
+use crate::game::board::Bitboard;
+
+pub struct Node {
+    pub board: Bitboard,
+    pub is_white: bool,
+    pub legal_moves: u64
+}
+
+impl Node {
+    pub fn new(board: Bitboard, is_white: bool, legal_moves: u64) -> Self {
+        Node {
+            board,
+            is_white,
+            legal_moves
+        }
+    }
+
+    pub fn apply_move(&self, mv: u8) -> Self {
+        let new_board = self.board.apply_move(mv, self.is_white);
+        let (new_player, legal_moves) = switch_player(&new_board, self.is_white);
+        
+        Node::new(new_board, new_player, legal_moves)
+    }
+}
+
+fn switch_player(board: &Bitboard, is_white: bool) -> (bool, u64) {
+    let opponent = !is_white;
+    let legal_moves = board.legal_moves(opponent);
+
+    match legal_moves == 0 {
+        true => (is_white, board.legal_moves(is_white)), // current player moves again
+        false => (opponent, legal_moves) // regular switch
+    }
+}

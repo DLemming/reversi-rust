7a1fb87ef25e5c2cce898ddb8e99041bc83ddb58
David Lemming <david.lemming28@gmail.com>
2025-05-17 18:43:51 +0200

    Add game_benchmark to test game runtime of two engines playing against each other

---
+++ benches/game_benchmark.rs
@@ -0,0 +1,42 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use reversi::engine::engine::Engine;
+use reversi::game::game::GameState;
+
+fn game_benchmark(c: &mut Criterion) {
+    let mut group = c.benchmark_group("minimax");
+
+    group.measurement_time(std::time::Duration::from_secs(10)); // or however long you want
+    group.sample_size(10); // Optional: how many samples Criterion collects
+
+    let depth = 9;
+    
+    group.bench_function("Run entire game", |b| {
+        b.iter(|| {
+            let engine = Engine::new(depth);
+            let mut game = GameState::new();
+
+            while !game.game_over() {
+                let mv: u8 = get_engine_move(&game, &engine);
+
+                game.apply_move(mv);
+                game.switch_player();
+            }
+
+            fn get_engine_move(game: &GameState, engine: &Engine) -> u8 {
+                let (_score, mv) = engine.search(game);
+
+                let mv: u8 = match mv {
+                    Some(mv) => mv,
+                    None => {
+                        println!("ERROR. Should not happen. Engine did not find a move!");
+                        0
+                    }
+                }; 
+                mv
+            }
+        });
+    });
+}
+
+criterion_group!(benches, game_benchmark);
+criterion_main!(benches);

4fa19981f0d42e763d14af7c61def6518d76038a
David Lemming <david.lemming28@gmail.com>
2025-05-18 20:53:28 +0200

    Inline always node functions. Significant performance boost

--- src/engine/node.rs
+++ src/engine/node.rs
@@ -15,6 +15,7 @@ impl Node {
         }
     }
 
+    #[inline(always)]
     pub fn apply_move(&self, mv: u64) -> Self {
         let new_board = self.board.apply_move(mv, self.is_white);
         let (new_player, legal_moves) = switch_player(&new_board, self.is_white);
@@ -23,6 +24,7 @@ impl Node {
     }
 }
 
+#[inline(always)]
 fn switch_player(board: &Bitboard, is_white: bool) -> (bool, u64) {
     let opponent = !is_white;
     let legal_moves = board.legal_moves(opponent);
